1ed8748f6ca6fa675be364ba9660a64cb84c42bb
greg <grzegorz.kozub@gmail.com>
2024-12-21 13:06:54 +0100

    combine the files using a script

---
+++ src/build.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/tidwall/gjson"
+	"github.com/tidwall/sjson"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+)
+
+func main() {
+	for _, variant := range [2]string{"dark", "light"} {
+		theme := fmt.Sprintf(
+			`{ "name": "Gruvbox Material Flat %s", "type": "%s" }`,
+			cases.Title(language.English).String(variant),
+			variant)
+
+		templateDir := "./templates/" + variant + "/"
+
+		colorsFile, err := os.ReadFile(templateDir + "colors.jsonc")
+		if err != nil {
+			panic(err)
+		}
+		colors := gjson.Get(string(colorsFile), "colors")
+
+		theme, err = sjson.SetRaw(theme, "colors", colors.Raw)
+		if err != nil {
+			panic(err)
+		}
+
+		tokenColorsFile, err := os.ReadFile(templateDir + "token-colors-sainnhe.json")
+		if err != nil {
+			panic(err)
+		}
+		tokenColors := gjson.Get(string(tokenColorsFile), "tokenColors")
+
+		theme, err = sjson.SetRaw(theme, "tokenColors", tokenColors.Raw)
+		if err != nil {
+			panic(err)
+		}
+
+		tokenColorsOverridesFile, err := os.ReadFile(templateDir + "token-colors-greg.json")
+		if err != nil {
+			panic(err)
+		}
+		tokenColorsOverrides := gjson.Get(string(tokenColorsOverridesFile), "tokenColors")
+
+		tokenColorsOverrides.ForEach(func(key, value gjson.Result) bool {
+			theme, err = sjson.SetRaw(theme, "tokenColors.-1", value.Raw)
+			if err != nil {
+				panic(err)
+			}
+			return true
+		})
+
+		theme = gjson.Get(theme, "@pretty").Raw
+
+		if err := os.WriteFile(
+			"../themes/gruvbox-material-flat-"+variant+"-color-theme.json", []byte(theme), 0644); err != nil {
+			panic(err)
+		}
+	}
+}
